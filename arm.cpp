84eb405546a0f47479b8513e92b73bc0e72df845
Jacob Greenway <jagreenway12@gmail.com>
2018-02-25 01:43:00 -0600

    straight up ballin

--- arm.cpp
+++ arm.cpp
@@ -2,25 +2,27 @@
 
 // Define step constraints
 #define STEP_SIZE 1.8
-#define MICROSTEP_MODE 32
+#define MICROSTEP_MODE 32.0
 
 // Arm lengths in meters
-#define ARM_LENGTH 0.2794
-#define ARM_EXTENSION_LENGTH 0.2794
+#define ARM_LENGTH 0.25
+#define EXTENSION_LENGTH 0.18
 
 const double ARM_GEAR_RATIO = 111.0 / 32.0;
-const double EXTENSION_GEAR_RATIO = 10.0 / 40.0;
+const double EXTENSION_GEAR_RATIO = 3.3;
 
 /**
  * Input degrees to move the stepper
  */
 static int degreesToSteps(double deg)
 {
-    return (deg / STEP_SIZE) * MICROSTEP_MODE;
+  deg = fabs(deg);
+  return (deg / STEP_SIZE) * MICROSTEP_MODE;
 }
 
-static double stepsToDegrees(int steps) {
-  return (steps / MICROSTEP_MODE) * STEP_SIZE;
+static double stepsToDegrees(int steps)
+{
+  return ((double)steps / MICROSTEP_MODE) * STEP_SIZE;
 }
 
 /**
@@ -29,7 +31,12 @@ static double stepsToDegrees(int steps) {
  */
 static double getArmAngle(double x, double y)
 {
-  return (ARM_EXTENSION_LENGTH + atan2(y, x) * 180 / M_PI) * ARM_GEAR_RATIO;
+
+  double hypotenuse = sqrt(x * x + y * y);
+  double numerator =  EXTENSION_LENGTH * EXTENSION_LENGTH + hypotenuse * hypotenuse - ARM_LENGTH * ARM_LENGTH;
+  double denominator = 2 * hypotenuse * EXTENSION_LENGTH;
+
+  return (acos(numerator / denominator) + atan2(y, x)) * 180 / M_PI;
 }
 
 /**
@@ -38,11 +45,11 @@ static double getArmAngle(double x, double y)
  */
 static double getExtensionAngle(double x, double y)
 {
-  double hypotenuse = sqrt(pow(x, 2) + pow(y, 2));
-  double numerator = pow(ARM_LENGTH, 2) + pow(ARM_EXTENSION_LENGTH, 2) - pow(hypotenuse, 2);
-  double denominator = 2 * ARM_LENGTH * ARM_EXTENSION_LENGTH;
+  double hypotenuse = sqrt(x * x + y * y);
+  double numerator = ARM_LENGTH * ARM_LENGTH + EXTENSION_LENGTH * EXTENSION_LENGTH - hypotenuse * hypotenuse;
+  double denominator = 2 * ARM_LENGTH * EXTENSION_LENGTH;
 
-  return (acos(numerator / denominator) * 180 / M_PI) * EXTENSION_GEAR_RATIO;
+  return acos(numerator / denominator) * 180 / M_PI;
 }
 
 /**
--- main.ino
+++ main.ino
@@ -13,13 +13,13 @@
 
 // Define direction pins
 #define ARM_DIR 3
-#define EXTENSION_DIR 8
-#define CLAW_DIR 6
+#define EXTENSION_DIR 6
+#define CLAW_DIR 8
 
 // Define step pins
 #define ARM_STEP 4
-#define EXTENSION_STEP 9
-#define CLAW_STEP 7
+#define EXTENSION_STEP 7
+#define CLAW_STEP 9
 
 // Global ethernet variables
 byte mac[] = {0x00, 0xAA, 0xBB, 0xCC, 0xDE, 0x02};
@@ -27,16 +27,14 @@ char server[] = "echo.websocket.org";
 int port = 80;
 
 // Global position variables
-int armSteps = 0;
-int extensionSteps = 0;
-int clawSteps = 0;
+int armSteps, extensionSteps, clawSteps = 0;
 
 double currentArmAngle = 0;
-double currentExtensionAngle = 0;
+double currentExtensionAngle = 180;
 double currentClawAngle = 0;
 
-double targetArmAngle = 45 * ARM_GEAR_RATIO;
-double targetExtensionAngle = 45 * EXTENSION_GEAR_RATIO;
+double targetArmAngle = 90;
+double targetExtensionAngle = currentExtensionAngle + (targetArmAngle / ARM_GEAR_RATIO);
 double targetClawAngle = 0;
 
 double posX, posY = 0;
@@ -65,22 +63,80 @@ void readSocket()
 
 void stepArm()
 {
-  if (armSteps < degreesToSteps(targetArmAngle))
+  const double diff = fabs(targetArmAngle - currentArmAngle);
+  if (diff > 0.1)
+  {
+    if (targetArmAngle > currentArmAngle)
+    {
+      step(false, ARM_DIR, ARM_STEP, 100);
+      currentArmAngle += stepsToDegrees(1) / ARM_GEAR_RATIO;
+    }
+    else if (targetArmAngle < currentArmAngle)
+    {
+      step(true, ARM_DIR, ARM_STEP, 100);
+      currentArmAngle -= stepsToDegrees(1) / ARM_GEAR_RATIO;
+    }
+  }
+  //Serial.print("arm angle: "); Serial.println(currentArmAngle);
+  /*
+  if (armSteps < degreesToSteps(targetArmAngle) * ARM_GEAR_RATIO)
   {
     boolean dir = checkAngle(currentArmAngle, targetArmAngle);
     step(dir, ARM_DIR, ARM_STEP, 100);
     armSteps++;
+    if (dir)
+    {
+      currentArmAngle += stepsToDegrees(1) * ARM_GEAR_RATIO;
+    }
+    else
+    {
+      currentArmAngle -= stepsToDegrees(1) * ARM_GEAR_RATIO;
+    }
   }
+  */
+  // else {
+  //   Serial.print("arm angle: "); Serial.println(currentArmAngle);
+  // }
 }
 
 void stepExtension()
 {
-  if (armSteps < degreesToSteps(targetExtensionAngle))
+  const double diff = fabs(targetExtensionAngle - currentExtensionAngle);
+  if (diff > 0.1)
+  {
+    if (targetExtensionAngle > currentExtensionAngle)
+    {
+      step(false, EXTENSION_DIR, EXTENSION_STEP, 100);
+      currentExtensionAngle += stepsToDegrees(1) / EXTENSION_GEAR_RATIO;
+    }
+    else if (targetExtensionAngle < currentExtensionAngle)
+    {
+      step(true, EXTENSION_DIR, EXTENSION_STEP, 100);
+      currentExtensionAngle -= stepsToDegrees(1) / EXTENSION_GEAR_RATIO;
+    }
+  }
+  //Serial.print("extension angle: "); Serial.println(currentExtensionAngle);
+  /*
+  if (extensionSteps < degreesToSteps(targetExtensionAngle) * EXTENSION_GEAR_RATIO)
   {
     boolean dir = checkAngle(currentExtensionAngle, targetExtensionAngle);
+    //Serial.print("Steps: "); Serial.println(extensionSteps);
     step(dir, EXTENSION_DIR, EXTENSION_STEP, 100);
+    //Serial.print("Target: "); Serial.println(degreesToSteps(targetExtensionAngle));
     extensionSteps++;
+    if (dir)
+    {
+      currentExtensionAngle += stepsToDegrees(1) * EXTENSION_GEAR_RATIO;
+    }
+    else
+    {
+      currentExtensionAngle -= stepsToDegrees(1) * EXTENSION_GEAR_RATIO;
+    }
   }
+  */
+  // else {
+  //   Serial.print("extension angle: "); Serial.println(currentExtensionAngle);
+  // }
 }
 
 void stepClaw()
@@ -112,6 +168,11 @@ void setup()
 {
   Serial.begin(9600);
 
+  Serial.print("arm angle: ");
+  Serial.println(targetArmAngle);
+  Serial.print("extension angle: ");
+  Serial.println(targetExtensionAngle);
+
   // Initialize ip connection
   if (Ethernet.begin(mac) == 0)
   {
@@ -135,13 +196,30 @@ void setup()
   // Start websocket client
   // client.begin();
 }
-
+int count = 0;
 void loop()
 {
   //readSocket();
   stepArm();
   stepExtension();
   stepClaw();
+
+  if (count >= 1000)
+  {
+    delay(500);
+    Serial.print("arm target: ");
+    Serial.println(targetArmAngle);
+    Serial.print("extension target: ");
+    Serial.println(targetExtensionAngle);
+    Serial.print("arm current: ");
+    Serial.println(currentArmAngle);
+    Serial.print("extension current: ");
+    Serial.println(currentExtensionAngle);
+    delay(500);
+    count = 0;
+  }
+
+  count++;
 
   lastX = posX;
   lastY = posY;
